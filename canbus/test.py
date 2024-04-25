b8036e8ad3f872bf5517c188e6c336a386aef6fb
voxum <daniel@torkhub.com>
2024-04-24 21:02:14 -0400

    can0

--- canbus/test.py
+++ canbus/test.py
@@ -1,7 +1,7 @@
 import can
 
 # Configure the channel and bitrate
-bus = can.interface.Bus(bustype='socketcan', channel='/dev/ttyCAN', bitrate=500000)
+bus = can.interface.Bus(bustype='socketcan', channel='can0', bitrate=500000)
 
 # Create a new CAN message
 msg = can.Message(arbitration_id=0x123, data=[0x11, 0x22, 0x33], is_extended_id=False)
