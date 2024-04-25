9b5cb85ce1330c3b6cb51607418d70af69a72701
voxum <daniel@torkhub.com>
2024-04-24 20:56:16 -0400

    tesster

---
+++ canbus/test.py
@@ -0,0 +1,14 @@
+import can
+
+# Configure the channel and bitrate
+bus = can.interface.Bus(bustype='socketcan', channel='ttyCAN', bitrate=500000)
+
+# Create a new CAN message
+msg = can.Message(arbitration_id=0x123, data=[0x11, 0x22, 0x33], is_extended_id=False)
+
+# Send the message
+try:
+    bus.send(msg)
+    print("Message sent on the bus!")
+except can.CanError:
+    print("Message NOT sent")
